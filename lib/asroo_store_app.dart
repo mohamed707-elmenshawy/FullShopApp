871292346897ab7be9792e775356590fdb44bcb3
mohamed.elmenshawy <mohamed.elmenshawy@57357.org>
2024-05-09 00:27:54 +0300

    test

---
+++ lib/asroo_store_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TestEnvType extends StatelessWidget {
+  const TestEnvType({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text('data'),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
+
 import 'package:flutter/material.dart';
+import 'package:store_app/asroo_store_app.dart';
 
-void main(){
-  runApp(const MyApp());
+import 'core/app/env.variables.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await EnvVariable.myinstance.init(envType: EnvTypeEnum.prod);
+runApp(MyApp());
 }
-
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return  MaterialApp(
+      debugShowCheckedModeBanner: EnvVariable.envtypev=='dev',
+      home: TestEnvType(),
+    );
   }
 }
