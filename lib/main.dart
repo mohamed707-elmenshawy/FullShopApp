6685fb16fb114137dbd138227c5959265d9d6c3b
mohamed.elmenshawy <mohamed.elmenshawy@57357.org>
2024-05-09 09:29:56 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@
 import 'package:flutter/material.dart';
 import 'package:store_app/asroo_store_app.dart';
 
-<<<<<<< HEAD
 void main() {
   runApp(const MyApp());
 }
 
-=======
-import 'core/app/env.variables.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +14,6 @@ void main() async {
   await EnvVariable.myinstance.init(envType: EnvTypeEnum.prod);
 runApp(MyApp());
 }
->>>>>>> af8ddf497d196431dac7aa9d9f8e774a0e6c1264
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
