25e92f0a7111fd80e21f42030c7b398e91597fcd
mohamed.elmenshawy <mohamed.elmenshawy@57357.org>
2024-05-09 11:36:47 +0300

    connectivty done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:store_app/asroo_store_app.dart';
 import 'package:store_app/core/app/env.variables.dart';
@@ -7,16 +6,14 @@ void main() {
   runApp(const MyApp());
 }
 
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: EnvVariable.envtypev=='dev',
-      home: TestEnvType(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: EnvVariable.myinstance.debugMode,
+      home: const AsrooStoreApp(),
     );
   }
 }
